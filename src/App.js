b19059dbc07e0ac17d8c12d71175998bc150f771
StrangerZXY <stranger808z@gmail.com>
2024-06-25 03:54:47 +0300

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import Header from "./components/Header";
+// import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../Header.css';
+
+const Header = () => {
+	return (
+		<header className="header">
+
+			<img src={require("../images/Logo Small.png")} alt="Logo" />
+
+			<nav>
+				<ul>
+					<li><a href="/#">Home</a></li>
+					<li><a href="/#">About</a></li>
+					<li><a href="/#">Menu</a></li>
+					<li><a href="/#">Reservations</a></li>
+					<li><a href="/#">Order Online</a></li>
+					<li><a href="/#">Login</a></li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
