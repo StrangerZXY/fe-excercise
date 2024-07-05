5809ec6fbcaf84695fecbad5299460e3b859a4db
StrangerZXY <stranger808z@gmail.com>
2024-07-05 07:53:54 +0300

    Updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,16 @@ const Footer = () => {
 			<div className="footerPicture">
 				<img src={require("../images/Logo Rectangle.png")} alt="Footer" />
 			</div>
-			<div className="footerBox"><h3 className="footerBoxTitle">Doormat<br />Navigation</h3>
+			<div className="footerBox">
+				<h3 className="footerBoxTitle">Doormat<br />Navigation</h3>
 				<p className="footerBoxList">Home<br />About<br />Menu<br />Reservations<br />Order Online<br />Login</p>
 			</div>
-			<div className="footerBox"><h3 className="footerBoxTitle">Contact</h3>
+			<div className="footerBox">
+				<h3 className="footerBoxTitle">Contact</h3>
 				<p className="footerBoxList">Address<br />Phone number<br />Email</p>
 			</div>
-			<div className="footerBox"><h3 className="footerBoxTitle">Social Media Links</h3>
+			<div className="footerBox">
+				<h3 className="footerBoxTitle">Social Media Links</h3>
 				<p className="footerBoxList">Address<br />Phone number<br />Email</p>
 			</div>
 		</footer>
